43ce1b0ff5cb13fb5be3d5b909ff9ecfca203b2d
Kevin Junyoung Sohn <kevinsohn@dhcp-10-250-190-143.harvard.edu>
2024-02-05 22:26:44 -0500

    bad update 2

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,8 +2,7 @@ import { Separator } from "@/components/ui/separator";
 import { TypographyH2 } from "@/components/ui/typography";
 import { createServerSupabaseClient } from "@/lib/server-utils";
 import { redirect } from "next/navigation";
-import AddSpeciesDialog from "./add-species-dialog";
-import SpeciesCard from "./species-card";
+import UserCard from "./users-card";
 
 export default async function SpeciesList() {
   // Create supabase server component client and obtain user session from stored cookie
@@ -18,30 +17,18 @@ export default async function SpeciesList() {
   }
 
   // Obtain the ID of the currently signed-in user
-  const sessionId = session.user.id;
 
-  const { data: species } = await supabase
-    .from("species")
-    .select("*, profiles (id, email, display_name, biography)")
-    .order("id", { ascending: false });
+  const { data: profiles } = await supabase.from("profiles").select("*").order("id", { ascending: false });
   // const { data: profiles } = await supabase.from("profiles").select("*").order("id", { ascending: false });
 
   return (
     <>
       <div className="mb-5 flex flex-wrap items-center justify-between gap-4">
         <TypographyH2>Species List</TypographyH2>
-        <AddSpeciesDialog userId={sessionId} />
       </div>
       <Separator className="my-4" />
       <div className="flex flex-wrap justify-center">
-        {species?.map((species) => (
-          <SpeciesCard
-            key={species.id}
-            species={species}
-            displayName={species.profiles?.display_name ?? ""}
-            userId={sessionId}
-          />
-        ))}
+        {profiles?.map((profiles) => <UserCard key={profiles.id} profiles={profiles} />)}
       </div>
     </>
   );
